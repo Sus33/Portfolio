5acee3aa7ed8bc6e06ffd859825df199476fce21
Sus33 <susannamkrtchan98@gmail.com>
2018-02-12 09:32:55 +0400

    .

--- spring_user_demo/src/main/java/am/itspace/spring_user_demo/repository/PostRepository.java
+++
@@ -1,8 +0,0 @@
-package am.itspace.spring_user_demo.repository;
-
-import am.itspace.spring_user_demo.model.Post;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostRepository extends JpaRepository<Post, Integer> {
-
-}
--- spring_user_demo/src/main/java/am/itspace/spring_user_demo/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package am.itspace.spring_user_demo.repository;
-
-import am.itspace.spring_user_demo.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Integer>{
-
-}
